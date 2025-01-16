0fe927c836a66d82d3b73a69af10c4042e179871
kaustubh <kaustubhpatil1211@gmail.com>
2025-01-16 17:20:00 +0530

    client added

--- src/app/Component/Data.tsx
+++ src/app/Component/Data.tsx
@@ -1,17 +1,30 @@
+
+"use client"
 import { createContext, ReactNode } from "react"
 import { useState } from "react";
 export const todoContex= createContext(null);
-const [todo , setTodo]=useState();
+
+export type todo={
+
+    Id: Number;
+    task:String;
+    complete:boolean;
+    date: Date; 
+}
 
 export const todoProvoider= (children:ReactNode)=>{
+    const [todo , setTodo]=useState();
     const handleTodo=(task:String)=>{
         setTodo(()=>{
             const newTodo ={
                 Id: Math.random().toString(),
                 task,
+                complete:false, 
                 date: new Date
             }
+            ...prev
         })
+        return newTodo;
     } 
 
 }
